9e347b053f9f5679b4b422fb7b8bc40f131b0e5b
Mykyta <mp091689@gmail.com>
2024-05-22 01:14:20 -0400

    implement skeleton

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,34 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="relative">
+    <div
+        class="sticky top-0 h-screen flex flex-col items-center justify-center bg-gradient-to-b from-green-200 to-blue-200">
+      <img src="@/assets/images/1stclassdelivery-bg.jpeg" alt="Background Image"
+           class="absolute w-full h-full object-cover filter blur-sm brightness-50">
+      <div class="absolute text-white">
+        <h2 class="text-4xl font-bold drop-shadow-md">First Class Delivery</h2>
+        <p class="mt-2 drop-shadow-md">We Are The Best</p>
+      </div>
+    </div>
+    <div
+        class="sticky top-0 h-screen flex flex-col items-center justify-center bg-gradient-to-b from-green-200 to-blue-200">
+      <img src="@/assets/images/1stclassdelivery-fleet-bg.jpg" alt="Background Image"
+           class="absolute w-full h-full object-cover filter blur-sm brightness-50">
+      <div class="absolute text-white">
+        <h2 class="text-4xl font-bold drop-shadow-md">Our Fleet</h2>
+        <p class="mt-2 drop-shadow-md">We have a lot of trucks</p>
+      </div>
+    </div>
+    <div
+        class="sticky top-0 h-screen flex flex-col items-center justify-center bg-gradient-to-b from-indigo-800 to-purple-800 text-white">
+      <img src="@/assets/images/1stclassdelivery-careers-bg.jpg" alt="Background Image"
+           class="absolute w-full h-full object-cover filter blur-sm brightness-50">
+      <div class="absolute text-white">
+        <h2 class="text-4xl font-bold">Careers</h2>
+        <p class="mt-2">Join us now</p>
+      </div>
+    </div>
   </main>
 </template>
