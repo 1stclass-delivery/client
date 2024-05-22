123cf206aa24ba887e090e18cdc74f9c8469bfdd
Mykyta <mp091689@gmail.com>
2024-05-22 10:22:24 -0400

    set real contacts

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -36,8 +36,9 @@
            class="absolute w-full h-full object-cover filter blur-sm brightness-50">
       <div class="absolute text-white">
         <h2 class="text-4xl font-bold">Contacts</h2>
-        <p class="mt-2">Call us: +1234567890</p>
-        <p class="mt-2">Text us: example@example.com</p>
+        <p class="mt-2"><a href="tel:+12167048163">Call us: <span class="underline font-bold">+1 (216) 704-8163</span></a></p>
+        <p class="mt-2"><a href="mailto:tao.partizan@gmail.com">Text us: <span class="underline font-bold">tao.partizan@gmail.com</span></a>
+        </p>
       </div>
     </div>
   </main>
