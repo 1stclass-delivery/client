54dc2c53374e09445edc3ff65081cbae8e1fec20
Mykyta <mp091689@gmail.com>
2024-05-22 10:14:38 -0400

    compress background images, match background colors, add contacts page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,8 +4,8 @@
 <template>
   <main class="relative">
     <div
-        class="sticky top-0 h-screen flex flex-col items-center justify-center bg-gradient-to-b from-green-200 to-blue-200">
-      <img src="@/assets/images/1stclassdelivery-bg.jpeg" alt="Background Image"
+        class="sticky top-0 h-screen flex flex-col items-center justify-center bg-gradient-to-b from-cyan-800 to-orange-950">
+      <img src="@/assets/images/1stclassdelivery-bg.jpg" alt="Background Image"
            class="absolute w-full h-full object-cover filter blur-sm brightness-50">
       <div class="absolute text-white">
         <h2 class="text-4xl font-bold drop-shadow-md">First Class Delivery</h2>
@@ -13,7 +13,7 @@
       </div>
     </div>
     <div
-        class="sticky top-0 h-screen flex flex-col items-center justify-center bg-gradient-to-b from-green-200 to-blue-200">
+        class="sticky top-0 h-screen flex flex-col items-center justify-center bg-gradient-to-b from-blue-800 to-gray-900">
       <img src="@/assets/images/1stclassdelivery-fleet-bg.jpg" alt="Background Image"
            class="absolute w-full h-full object-cover filter blur-sm brightness-50">
       <div class="absolute text-white">
@@ -22,12 +22,22 @@
       </div>
     </div>
     <div
-        class="sticky top-0 h-screen flex flex-col items-center justify-center bg-gradient-to-b from-indigo-800 to-purple-800 text-white">
+        class="sticky top-0 h-screen flex flex-col items-center justify-center bg-gradient-to-b from-blue-800 to-green-900">
       <img src="@/assets/images/1stclassdelivery-careers-bg.jpg" alt="Background Image"
            class="absolute w-full h-full object-cover filter blur-sm brightness-50">
       <div class="absolute text-white">
         <h2 class="text-4xl font-bold">Careers</h2>
         <p class="mt-2">Join us now</p>
+      </div>
+    </div>
+    <div
+        class="sticky top-0 h-screen flex flex-col items-center justify-center bg-gradient-to-b from-lime-800 to-gray-800">
+      <img src="@/assets/images/1stclassdelivery-contacts-bg.jpg" alt="Background Image"
+           class="absolute w-full h-full object-cover filter blur-sm brightness-50">
+      <div class="absolute text-white">
+        <h2 class="text-4xl font-bold">Contacts</h2>
+        <p class="mt-2">Call us: +1234567890</p>
+        <p class="mt-2">Text us: example@example.com</p>
       </div>
     </div>
   </main>
