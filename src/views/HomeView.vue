edca65bb613bad3c4627bf373813b0fa0aa44e0e
Mykyta <mp091689@gmail.com>
2024-05-22 13:08:28 -0400

    add page padding, fix menu

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,8 +8,8 @@
         class="sticky top-0 h-screen flex flex-col items-center justify-center bg-gradient-to-b from-cyan-800 to-orange-950">
       <img src="@/assets/images/1stclassdelivery-bg.jpg" alt="Background Image"
            class="absolute w-full h-full object-cover filter blur-sm brightness-50">
-      <div class="absolute text-white max-w-screen-md">
-        <h2 class="text-6xl font-bold drop-shadow-md">First Class Delivery</h2>
+      <div class="absolute text-white text-sm md:text-base max-w-screen-md px-4">
+        <h2 class="text-4xl md:text-6xl font-bold drop-shadow-md">First Class Delivery</h2>
         <p class="mt-4 drop-shadow-md">First Class Delivery dedicated to providing exceptional transportation services
           across the United States and Canada.</p>
         <p class="mt-4 drop-shadow-md">Our trucks are equipped to handle a wide range of delivery needs, from small
@@ -26,8 +26,8 @@
         class="sticky top-0 h-screen flex flex-col items-center justify-center bg-gradient-to-b from-blue-800 to-gray-900">
       <img src="@/assets/images/1stclassdelivery-fleet-bg.jpg" alt="Background Image"
            class="absolute w-full h-full object-cover filter blur-sm brightness-50">
-      <div class="absolute text-white max-w-screen-md">
-        <h2 class="text-6xl font-bold drop-shadow-md">Fleet</h2>
+      <div class="absolute text-white text-sm md:text-base max-w-screen-md px-4">
+        <h2 class="text-4xl md:text-6xl font-bold drop-shadow-md">Fleet</h2>
         <p class="mt-4 drop-shadow-md">Our fleet consists straight trucks, box trucks, vans and semis with diverse
           brands like Freightliner, International, Volvo, Mercedes.</p>
         <p class="mt-4 drop-shadow-md">All trucks are well-maintained, regularly serviced, and equipped with the latest
@@ -40,8 +40,8 @@
         class="sticky top-0 h-screen flex flex-col items-center justify-center bg-gradient-to-b from-blue-800 to-green-900">
       <img src="@/assets/images/1stclassdelivery-careers-bg.jpg" alt="Background Image"
            class="absolute w-full h-full object-cover filter blur-sm brightness-50">
-      <div class="absolute text-white max-w-screen-md">
-        <h2 class="text-6xl font-bold">Career</h2>
+      <div class="absolute text-white text-sm md:text-base max-w-screen-md px-4">
+        <h2 class="text-4xl md:text-6xl font-bold">Career</h2>
         <p class="mt-4 drop-shadow-md">At First Class Delivery, we're committed to building a team of dedicated
           professionals who share our passion for delivering exceptional customer service.</p>
         <p class="mt-4 drop-shadow-md">If you're looking for a
@@ -56,10 +56,11 @@
         class="sticky top-0 h-screen flex flex-col items-center justify-center bg-gradient-to-b from-lime-800 to-gray-800">
       <img src="@/assets/images/1stclassdelivery-contacts-bg.jpg" alt="Background Image"
            class="absolute w-full h-full object-cover filter blur-sm brightness-50">
-      <div class="absolute text-white max-w-screen-md">
-        <h2 class="text-6xl font-bold">Contacts</h2>
+      <div class="absolute text-white text-sm md:text-base max-w-screen-md px-4">
+        <h2 class="text-4xl md:text-6xl font-bold">Contacts</h2>
         <p class="mt-4 drop-shadow-md">Contact us today to learn more about our services and how we can tailor our
-          transportation solutions to meet your unique needs. Trust First Class Delivery for all your transportation
+          transportation solutions to meet your unique needs.</p>
+        <p class="mt-4 drop-shadow-md">Trust First Class Delivery for all your transportation
           needs and experience the difference of working with a reliable and dedicated partner.</p>
         <p class="mt-4 drop-shadow-md"><a href="tel:+12167048163">Call us: <span
             class="underline font-bold">+1 (216) 704-8163</span></a></p>
