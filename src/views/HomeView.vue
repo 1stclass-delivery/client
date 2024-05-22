bb09ff172e537764d75f54f865eb875304146235
Mykyta <mp091689@gmail.com>
2024-05-22 11:10:55 -0400

    add content filler

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,37 +7,60 @@
         class="sticky top-0 h-screen flex flex-col items-center justify-center bg-gradient-to-b from-cyan-800 to-orange-950">
       <img src="@/assets/images/1stclassdelivery-bg.jpg" alt="Background Image"
            class="absolute w-full h-full object-cover filter blur-sm brightness-50">
-      <div class="absolute text-white">
-        <h2 class="text-4xl font-bold drop-shadow-md">First Class Delivery</h2>
-        <p class="mt-2 drop-shadow-md">We Are The Best</p>
+      <div class="absolute text-white max-w-screen-md">
+        <h2 class="text-6xl font-bold drop-shadow-md">First Class Delivery</h2>
+        <p class="mt-2 drop-shadow-md">First Class Delivery dedicated to providing exceptional transportation services
+          across the United States and Canada.</p>
+        <p class="mt-2 drop-shadow-md">Our trucks are equipped to handle a wide range of delivery needs, from small
+          packages to large quantities of goods.</p>
+        <p class="mt-2 drop-shadow-md">With a strong focus on customer satisfaction, First Class Delivery team of
+          experienced drivers and logistics professionals work tirelessly to ensure timely and efficient delivery of
+          your goods.</p>
+        <p class="mt-2 drop-shadow-md">Comprehensive network of routes covers major cities and towns across North
+          America, allowing to reach even the most remote locations with ease.</p>
       </div>
     </div>
     <div
         class="sticky top-0 h-screen flex flex-col items-center justify-center bg-gradient-to-b from-blue-800 to-gray-900">
       <img src="@/assets/images/1stclassdelivery-fleet-bg.jpg" alt="Background Image"
            class="absolute w-full h-full object-cover filter blur-sm brightness-50">
-      <div class="absolute text-white">
-        <h2 class="text-4xl font-bold drop-shadow-md">Our Fleet</h2>
-        <p class="mt-2 drop-shadow-md">We have a lot of trucks</p>
+      <div class="absolute text-white max-w-screen-md">
+        <h2 class="text-6xl font-bold drop-shadow-md">Fleet</h2>
+        <p class="mt-2 drop-shadow-md">Our fleet consists straight trucks, box trucks, vans and semis with diverse
+          brands like Freightliner, International, Volvo, Mercedes.</p>
+        <p class="mt-2 drop-shadow-md">All trucks are well-maintained, regularly serviced, and equipped with the latest
+          technology to ensure safe and reliable transportation.</p>
+        <p class="mt-2 drop-shadow-md">We've got the capacity to handle your most complex delivery needs.</p>
       </div>
     </div>
     <div
         class="sticky top-0 h-screen flex flex-col items-center justify-center bg-gradient-to-b from-blue-800 to-green-900">
       <img src="@/assets/images/1stclassdelivery-careers-bg.jpg" alt="Background Image"
            class="absolute w-full h-full object-cover filter blur-sm brightness-50">
-      <div class="absolute text-white">
-        <h2 class="text-4xl font-bold">Careers</h2>
-        <p class="mt-2">Join us now</p>
+      <div class="absolute text-white max-w-screen-md">
+        <h2 class="text-6xl font-bold">Careers</h2>
+        <p class="mt-4 drop-shadow-md">At First Class Delivery, we're committed to building a team of dedicated
+          professionals who share our passion for delivering exceptional customer service.</p>
+        <p class="mt-4 drop-shadow-md">If you're looking for a
+          challenging and rewarding career in transportation, we invite you to explore our current job
+          opportunities.</p>
+        <p class="mt-4 drop-shadow-md">We're proud to offer a range of career paths for professional drivers, from
+          experienced veterans to new entrants in the industry.</p>
       </div>
     </div>
     <div
         class="sticky top-0 h-screen flex flex-col items-center justify-center bg-gradient-to-b from-lime-800 to-gray-800">
       <img src="@/assets/images/1stclassdelivery-contacts-bg.jpg" alt="Background Image"
            class="absolute w-full h-full object-cover filter blur-sm brightness-50">
-      <div class="absolute text-white">
-        <h2 class="text-4xl font-bold">Contacts</h2>
-        <p class="mt-2"><a href="tel:+12167048163">Call us: <span class="underline font-bold">+1 (216) 704-8163</span></a></p>
-        <p class="mt-2"><a href="mailto:tao.partizan@gmail.com">Text us: <span class="underline font-bold">tao.partizan@gmail.com</span></a>
+      <div class="absolute text-white max-w-screen-md">
+        <h2 class="text-6xl font-bold">Contacts</h2>
+        <p class="mt-4 drop-shadow-md">Contact us today to learn more about our services and how we can tailor our
+          transportation solutions to meet your unique needs. Trust First Class Delivery for all your transportation
+          needs and experience the difference of working with a reliable and dedicated partner.</p>
+        <p class="mt-4 drop-shadow-md"><a href="tel:+12167048163">Call us: <span
+            class="underline font-bold">+1 (216) 704-8163</span></a></p>
+        <p class="mt-4 drop-shadow-md"><a href="mailto:tao.partizan@gmail.com">Text us: <span
+            class="underline font-bold">tao.partizan@gmail.com</span></a>
         </p>
       </div>
     </div>
