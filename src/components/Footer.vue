f89edcdb435f726e964ef2cc7a5463f2353ca786
Mykyta <mp091689@gmail.com>
2024-07-28 12:57:34 -0400

    add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <footer class="fixed bottom-0 left-0 w-full text-white font-medium z-10 p-1 backdrop-blur-sm bg-gray-700/25">
+    <div class="flex grid-cols-2">
+      <img src="@/assets/images/fcd-logo.png" alt="logo" class="max-w-64 md:hidden">
+      <div class="w-full flex justify-end">
+      </div>
+    </div>
+    <ul class="grid grid-cols-1 md:flex md:justify-evenly gap-y-4 mt-4 md:mt-0">
+      <li>©2024 First Class Delivery LLC</li>
+    </ul>
+  </footer>
+</template>
+
+<style scoped>
+
+</style>
