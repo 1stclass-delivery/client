f1baa52ded402393fac7487dbeb74d36f818c839
Mykyta Popov <mp091689@gmail.com>
2024-12-19 10:10:19 -0500

    Update StickySection.vue

--- src/components/StickySection.vue
+++ src/components/StickySection.vue
@@ -5,7 +5,7 @@ defineProps<{ id: string, bgImage: string }>();
 <template>
   <section :id="id"
            :style="{ backgroundImage: 'url(' + bgImage + ')' }"
-           class="sticky top-0 text-white text-sm md:text-base h-screen snap-start overflow-scroll bg-cover bg-center">
+           class="sticky top-0 text-white text-sm md:text-base h-screen snap-start bg-cover bg-center">
     <div class="w-11/12 md:w-3/4 m-auto">
       <div class="h-28 md:h-16"></div>
       <slot name="header"></slot>
